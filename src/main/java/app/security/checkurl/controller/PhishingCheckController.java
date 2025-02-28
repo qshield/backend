00f47a63a0d1db9151b989e0830ba1baed7246c8
iamjames77 <iamjames77@daum.net>
2025-03-01 01:01:31 +0900

    오류해결

--- src/main/java/app/security/checkurl/controller/PhishingCheckController.java
+++ src/main/java/app/security/checkurl/controller/PhishingCheckController.java
@@ -1,8 +1,14 @@
 package app.security.checkurl.controller;
 
-import app.security.checkurl.service;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.security.checkurl.service.PhishingCheckService;
 
 @RestController
 @RequestMapping("/api")
